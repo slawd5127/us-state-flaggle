43cb7ab3bf1501b452c636afea48133886b9f288
slawd <116815339+slawd5127@users.noreply.github.com>
2023-01-17 13:36:03 +0000

    added a class

---
+++ script.js
@@ -0,0 +1,16 @@
+class State {
+  constructor(flag, name, long, lat) {
+    this.flag = flag;
+    this.name = name;
+    this.long = lat;
+    this.lat = long;
+  }
+}
+
+let alabama = new State("image", "Alabama", 32.366805, -86.299969);
+let alaska = new State("image", "Alaska", 58.301944, -134.419722);
+let arizona = new State("image", "Arizona", 33.448377, -112.074037); 
+let arkansas = new State("image", "Arkansas", 34.746481, -92.289595);
+
+
+
