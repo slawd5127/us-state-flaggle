e418b11292e26b8bdd4043bd55358696e42c3912
slawd <116815339+slawd5127@users.noreply.github.com>
2023-01-23 14:35:36 +0000

    added more states, input box, submit button,  filtering search box

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ class State {
 }
 
 class Round {
-  constructor(correctState){
+  constructor(correctState) {
     this.correctState = correctState;
     this.guesses = [];
-    
+
   }
-  
+
 }
 
 class Game {
-  constructor(listofStates){
+  constructor(listofStates) {
     this.listofStates = listofStates;
     this.playedStates = [];
   }
@@ -32,9 +32,9 @@ function preload() {
   let arizona = new State(loadImage('images/arizona.jpg'), "Arizona", 33.448377, -112.074037);
   let arkansas = new State(loadImage('images/arkansas.jpg'), "Arkansas", 34.746481, -92.289595);
   let california = new State(loadImage('images/california.jpg'), "California", 38.581572, -121.494400);
-  let colorado = new State(loadImage('images/colorado.jpg'), "Colorado", 39.739236, 	-104.990251)
+  let colorado = new State(loadImage('images/colorado.jpg'), "Colorado", 39.739236, -104.990251)
 
-let allStates = [alabama, alaska, arizona, arkansas, california, colorado]
+  let allStates = [alabama, alaska, arizona, arkansas, california, colorado]
 
   game = new Game(allStates);
 }
@@ -46,70 +46,116 @@ function setup() {
 
   frameRate(1)
 
-  newRound() 
+  newRound()
 
-  
-  
-  
+  createControls()
 
 }
 
-function newRound(){
+function newRound() {
   let state = random(game.listofStates)
   currentRound = new Round(state)
-  
+
 }
 
-function draw(){
+function draw() {
   //title
+  strokeWeight(3);
   fill(0, 0, 0,);
   textAlign(CENTER);
   textSize(20);
   text("US STATE FLAGGLE", 220, 20);
-  
+
   //outline
   strokeWeight(4);
   stroke(51);
   noFill();
   rect(44, 44, 352, 235);
-  
+
   //flag
   print(currentRound.correctState.name);
   image(currentRound.correctState.flag, 45, 45, 350, 233);
 
   // guess boxes 
   strokeWeight(2);
-  fill(200,200,200)
+  fill(200, 200, 200)
   rect(45, 300, 350, 30);
   rect(45, 350, 350, 30);
   rect(45, 400, 350, 30);
   rect(45, 450, 350, 30);
   rect(45, 500, 350, 30);
-  
-  //input box
+
+}
+
+
+
+function createControls(){
+   //input box
   let inp = createInput('');
   inp.position(50, 550);
   inp.size(355, 30);
+
+  //button
   button = createButton('submit');
   button.position(370, 550);
   button.size(55, 30)
   button.mousePressed(InputEvent);
 
-
-
   
-  // input = createInput();
-  // input.position(45, 550, 350, 30);
-
   
-  //rect(object.style.textAlign = "center");
-  
-
-  // input box
-  // submit button
 }
 
-function InputEvent(){
+function generateSelection(filter){
+  if(sel) {sel.remove()}
+  sel = createSelect()
+  sel.postion(50, 540);
+  allStates.forEach(state => {
+    if(state.name.startsWith(filter)){
+      sel.option(state.name)
+      sel.changed(myselectEvent)
+    }
+  })
+}
+
+function myselectEvent(){
+  let statename = sel.value()
+  if(sel.value = correctState.name){
     
-}
+  }
+    
 
+
+
+
+function InputEvent() {
+ if (inp = correctState.name) {
+    
+   
+    
+
+
+  }
+}
+    
+    
+
+
+//  p5.prototype.popup = (
+//     message,
+//     seconds = 1,
+//     x = width / 2,
+//     y = height / 2,
+//     size = Math.max(innerWidth, innerHeight) / 50
+// ) => {
+//     let stop_frame_number = seconds * frameRate();
+//     if (frameCount <= stop_frame_number) {
+//         let opacity_value = floor(
+//             map(
+//                 abs(frameCount - stop_frame_number),
+//                 0,
+//                 stop_frame_number,
+//                 0,
+//                 255
+//             )
+//         );
+} 
